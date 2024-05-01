6cbc9d4e2c36db0526e1e8d4f1fec62d217cd79e
dyahstyn <95192159+dyahstyn@users.noreply.github.com>
2024-05-01 20:35:22 +0000

    commit & push

--- authorship-verification-trivial/authorship_verification_trivial.py
+++ authorship-verification-trivial/authorship_verification_trivial.py
@@ -1,7 +1,24 @@
 from pathlib import Path
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer 
+from sklearn.linear_model import LogisticRegression
 
 from tira.rest_api_client import Client
 from tira.third_party_integrations import get_output_directory
+
+def extract_tfidf_features(text_data, tfidf_vectorizer=None):
+   
+    if tfidf_vectorizer is None:
+         tfidfVectorizer = TfidfVectorizer(max_features=1000)
+    tfidf_features = tfidf_vectorizer.fit_transform(text_data)
+    return tfidf_features
+
+#example usage 
+text_train= {'text':['example text 1 ','example tex 2']}
+
+tfidf_vectorizer = TfidfVectorizer(max_features=1000)
+tfidf_features_train = extract_tfidf_features(text_train['text'],tfidf_vectorizer)
+print(tfidf_features_train)
 
 if __name__ == "__main__":
 
@@ -21,20 +38,24 @@ if __name__ == "__main__":
     targets_validation = tira.pd.truths(
         "nlpbuw-fsu-sose-24", "authorship-verification-validation-20240408-training"
     )
+    # Extract TF-TDF features 
+    tfidf_features_train = extract_tfidf_features(text_train['text'])
+    tfidf_features_validation = extract_tfidf_features(text_validation['text'])
 
-    # classifying the data
-    prediction = (
-        text_validation.set_index("id")["text"]
-        .str.contains("delve", case=False)
-        .astype(int)
-    )
+    #train classifier 
+    classifier = LogisticRegression()
 
-    # converting the prediction to the required format
-    prediction.name = "generated"
-    prediction = prediction.reset_index()
+    classifier.fit(tfidf_features_train , targets_train['generated'])
+
+    #predictions 
+
+    predictions = classifier.predict(tfidf_features_validation)
+
+    
 
     # saving the prediction
     output_directory = get_output_directory(str(Path(__file__).parent))
-    prediction.to_json(
+    predictions_df=pd.DataFrame({'id':text_validation['id'] , 'generated':predictions})
+    predictions_df.to_json(
         Path(output_directory) / "predictions.jsonl", orient="records", lines=True
     )
