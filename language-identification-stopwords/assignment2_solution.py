452ed036d2e65051217b7defbb22fe67fff1cd16
dyahstyn <95192159+dyahstyn@users.noreply.github.com>
2024-05-15 17:00:26 +0000

    assignment 2

---
+++ language-identification-stopwords/assignment2_solution.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+import re
+
+from tqdm import tqdm
+import pandas as pd
+from tira.rest_api_client import Client
+from tira.third_party_integrations import get_output_directory
+
+if __name__ == "__main__":
+
+    tira = Client()
+
+    # loading validation data (automatically replaced by test data when run on tira)
+    text_validation = tira.pd.inputs(
+        "nlpbuw-fsu-sose-24", "language-identification-validation-20240429-training"
+    )
+    targets_validation = tira.pd.truths(
+        "nlpbuw-fsu-sose-24", "language-identification-validation-20240429-training"
+    )
+
+    lang_ids = [
+        "af",
+        "az",
+        "bg",
+        "cs",
+        "da",
+        "de",
+        "el",
+        "en",
+        "es",
+        "fi",
+        "fr",
+        "hr",
+        "it",
+        "ko",
+        "nl",
+        "no",
+        "pl",
+        "ru",
+        "ur",
+        "zh",
+    ]
+
+    def load_stopwords(lang_ids):
+        stopwords = {}
+        for lang_id in lang_ids:
+            stopwords[lang_id] = set(
+            (Path(__file__).parent / "stopwords" / f"stopwords-{lang_id}.txt").read_text().splitlines()
+        ) - {"(", ")", "*", "|", "+", "?"}
+        return stopwords
+    
+    # classifying the data
+    def classify_language(text_validation, stopwords):
+        stopword_fractions = []
+        for lang_id in tqdm(stopwords.keys(), desc="Classifying Languages"):
+            lang_stopwords = stopwords[lang_id]
+            counts = pd.Series(0, index=text_validation.index, name=lang_id)
+            for stopword in lang_stopwords:
+                counts += (
+                    text_validation["text"]
+                    .str.contains(stopword, regex=False, case=False)
+                    .astype(int)
+                )
+            stopword_fractions.append(counts / len(lang_stopwords))
+        return pd.concat(stopword_fractions, axis=1)
+
+    stopwords = load_stopwords(lang_ids)
+
+    stopword_fractions = classify_language(text_validation, stopwords)
+
+    prediction = stopword_fractions.idxmax(axis=1)
+    prediction.name = "lang"
+    prediction = pd.concat([text_validation["id"], prediction], axis=1)
+
+    # saving the prediction
+    output_directory = get_output_directory(str(Path(__file__).parent))
+    prediction.to_json(
+        Path(output_directory) / "predictions.jsonl", orient="records", lines=True
+    )
