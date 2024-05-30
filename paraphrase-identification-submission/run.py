8fa7550c16e20f2b80e2a8718a834ceacfa0f333
Nailia Mirzakhmedova <nailia.mirzakhmedova@uni-weimar.de>
2024-05-30 10:56:55 +0200

    fix run py

--- paraphrase-identification-submission/run.py
+++ paraphrase-identification-submission/run.py
@@ -6,53 +6,59 @@ import pandas as pd
 from joblib import load
 from sklearn.metrics import accuracy_score,matthews_corrcoef
 from tira.third_party_integrations import get_output_directory
+from tira.rest_api_client import Client
 
 
 #reading the data to be tested from text.jsonl
-with open('text.jsonl', 'r') as f:
-    texts = [json.loads(line) for line in f]
-texts_df = pd.DataFrame(texts)
-texts_df['combined'] = texts_df['sentence1'] + ' ' + texts_df['sentence2']
+# with open('text.jsonl', 'r') as f:
+#     texts = [json.loads(line) for line in f]
+# texts_df = pd.DataFrame(texts)
+if __name__ == "__main__":
+    tira = Client()
+    texts_df = tira.pd.inputs(
+        "nlpbuw-fsu-sose-24", "paraphrase-identification-validation-20240515-training"
+    )
+    texts_df['combined'] = texts_df['sentence1'] + ' ' + texts_df['sentence2']
 
-#loading the dataset created using train.py
-pipeline = load('model.joblib')
+    #loading the dataset created using train.py
+    pipeline = load('model.joblib')
 
-predictions = pipeline.predict(texts_df['combined'])
+    predictions = pipeline.predict(texts_df['combined'])
 
-output = [{'id': int(row['id']), 'label': int(pred)} for row, pred in zip(texts_df.to_dict('records'), predictions)]
-
-
-output_directory = get_output_directory(str(Path(__file__).parent))
-
-output_file_path = Path(output_directory) / 'predictions.jsonl'
-with open(output_file_path, 'w') as f:
-    for pred in output:
-        f.write(json.dumps(pred) + '\n')
-
-#after the predictions are made we need to read both files to calculate the accuracy scr and  mcc        
-
-#reading truth labels given as input
-with open('labels.jsonl', 'r') as f:
-    true_labels = [json.loads(line) for line in f]
-true_labels_df = pd.DataFrame(true_labels)
-
-#reading labels predicted by the model
-with open('allpredictions.jsonl', 'r') as f:
-    predictions = [json.loads(line) for line in f]
-predictions_labels_df = pd.DataFrame(predictions)
+    output = [{'id': int(row['id']), 'label': int(pred)} for row, pred in zip(texts_df.to_dict('records'), predictions)]
 
 
-#just to make sure if the the id are of type int converting them once again to type:int
-true_labels_df['id'] = true_labels_df['id'].astype(int)
-predictions_labels_df['id'] = predictions_labels_df['id'].astype(int)
+    output_directory = get_output_directory(str(Path(__file__).parent))
 
-merged_df = pd.merge(true_labels_df, predictions_labels_df, on='id', suffixes=('_true', '_pred'))
+    output_file_path = Path(output_directory) / 'predictions.jsonl'
+    with open(output_file_path, 'w') as f:
+        for pred in output:
+            f.write(json.dumps(pred) + '\n')
 
-# Calculate the accuracy score
-accuracy = accuracy_score(merged_df['label_true'], merged_df['label_pred'])
-print(f"Accuracy: {accuracy:.4f}")
+# #after the predictions are made we need to read both files to calculate the accuracy scr and  mcc        
+
+# #reading truth labels given as input
+# with open('labels.jsonl', 'r') as f:
+#     true_labels = [json.loads(line) for line in f]
+# true_labels_df = pd.DataFrame(true_labels)
+
+# #reading labels predicted by the model
+# with open('allpredictions.jsonl', 'r') as f:
+#     predictions = [json.loads(line) for line in f]
+# predictions_labels_df = pd.DataFrame(predictions)
 
 
-#claculating Mathews Correlation Coefficient
-mcc = matthews_corrcoef(merged_df['label_true'], merged_df['label_pred'])
-print(f"Matthews Correlation Coefficient: {mcc:.4f}")
+# #just to make sure if the the id are of type int converting them once again to type:int
+# true_labels_df['id'] = true_labels_df['id'].astype(int)
+# predictions_labels_df['id'] = predictions_labels_df['id'].astype(int)
+
+# merged_df = pd.merge(true_labels_df, predictions_labels_df, on='id', suffixes=('_true', '_pred'))
+
+# # Calculate the accuracy score
+# accuracy = accuracy_score(merged_df['label_true'], merged_df['label_pred'])
+# print(f"Accuracy: {accuracy:.4f}")
+
+
+# #claculating Mathews Correlation Coefficient
+# mcc = matthews_corrcoef(merged_df['label_true'], merged_df['label_pred'])
+# print(f"Matthews Correlation Coefficient: {mcc:.4f}")
